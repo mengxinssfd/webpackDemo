4bec0ea4ad7aad5bac3a7148a6bdc3f4d816e85a
minipc <xinzon32177@163.com>
2019-02-08 12:54:55 +0800

    from home

--- demo5-css/webpack.config.js
+++ demo5-css/webpack.config.js
@@ -1,141 +1,152 @@
 var path = require('path');
 var CleanPlugin = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var Autoprefixer = require("autoprefixer")
 /*var extractStylus = new ExtractTextPlugin({
   filename: '[name].min.css',
   allChunks: true,
 });*/
 module.exports = {
-  // context: path.join(__dirname, 'src'),
-  mode: 'production',
-  entry: {
-    app: './src/app.ts',
-    // app: './src/main.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './dist/',
-    filename: 'js/[name].js',
-  },
-  module: {
-    rules: [
-      // css
-      {
-        test: /\.css$/,
-        use: [
-          // 执行顺序从下往上
-          {
-            // loader: 'style-loader/url',// import一次生成一个文件,使用link标签，与file-loader搭配
-            loader: 'style-loader',// 使用script标签，直接生成到html里去
-            options: {
-              // singleton: true,// 全部css合并到单个script标签
-              // transform: './css.transform.js',// 在浏览器环境运行
-              // insertInto: '#app',// script插到#app元素中
+    // context: path.join(__dirname, 'src'),
+    mode: 'production',
+    entry: {
+        app: './src/app.ts',
+        // app: './src/main.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './dist/',
+        filename: 'js/[name].js',
+    },
+    module: {
+        rules: [
+            // css
+            {
+                test: /\.css$/,
+                use: [
+                    // 执行顺序从下往上
+                    {
+                        // loader: 'style-loader/url',// import一次生成一个文件,使用link标签，与file-loader搭配
+                        loader: 'style-loader',// 使用script标签，直接生成到html里去
+                        options: {
+                            // singleton: true,// 全部css合并到单个script标签
+                            // transform: './css.transform.js',// 在浏览器环境运行
+                            // insertInto: '#app',// script插到#app元素中
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            // minimize: true,// v1.0.0已移除 // remove minimize option, use postcss-loader with cssnano or use optimize-cssnano-plugin plugin
+                            modules: true,
+                            localIdentName: '[path][name]_[local]_[hash:4]',// class name
+                            // alias: 'test-[name]',
+                        },
+                        // loader: 'file-loader',
+                    },
+                ],
             },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              // minimize: true,// v1.0.0已移除 // remove minimize option, use postcss-loader with cssnano or use optimize-cssnano-plugin plugin
-              modules: true,
-              localIdentName: '[path][name]_[local]_[hash:4]',
-              // alias: 'test-[name]',
+            // less
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'less-loader',
+                    },
+                ],
             },
-            // loader: 'file-loader',
-          },
-        ],
-      },
-      // less
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'less-loader',
-          },
-        ],
-      },
-      // sass/scss
-      {
-        test: /\.(sass|scss)$/,
+            // sass/scss
+            {
+                test: /\.(sass|scss)$/,
 
-        /*use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          }],*/
-        // use: extractStylus.extract([/*'style-loader',*/ 'css-loader', 'sass-loader']),
-        use: ExtractTextPlugin.extract({
-          fallback: {loader: 'style-loader'},
-          use: [
-            {
-              loader: 'css-loader',
+                /*use: [
+                  {
+                    loader: 'style-loader',
+                  },
+                  {
+                    loader: 'css-loader',
+                  },
+                  {
+                    loader: 'sass-loader',
+                  }],*/
+                // use: extractStylus.extract([/*'style-loader',*/ 'css-loader', 'sass-loader']),
+                use: ExtractTextPlugin.extract({
+                    fallback: {loader: 'style-loader'},
+                    use: [
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'sass-loader',
+                        }],
+                }),
             },
-            {
-              loader: 'sass-loader',
-            }],
-        }),
-      },
-      // stylus
-      // 不使用ExtractTextPlugin
-      /* {
-         test: /\.styl$/,
+            // stylus
+            // 不使用ExtractTextPlugin
+            /* {
+               test: /\.styl$/,
 
-         use: [
-           {
-             loader: 'style-loader',
-           },
-           {
-             loader: 'css-loader',
-           },
-           {
-             loader: 'stylus-loader',
-           }],
-       },*/
-      // 使用ExtractTextPlugin
-      {
-        test: /\.styl$/,
-        // use: extractStylus.extract([/*'style-loader',*/ 'css-loader', 'stylus-loader']),
-        use: ExtractTextPlugin.extract({
-          fallback: {loader: 'style-loader'},
-          use: [
+               use: [
+                 {
+                   loader: 'style-loader',
+                 },
+                 {
+                   loader: 'css-loader',
+                 },
+                 {
+                   loader: 'stylus-loader',
+                 }],
+             },*/
+            // 使用ExtractTextPlugin
             {
-              loader: 'css-loader',
+                test: /\.styl$/,
+                // use: extractStylus.extract([/*'style-loader',*/ 'css-loader', 'stylus-loader']),
+                use: ExtractTextPlugin.extract({
+                    fallback: {loader: 'style-loader'},
+                    use: [
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                ident:"postcss",
+                                plugins:[
+                                    // Autoprefixer(),
+                                    require("postcss-cssnext")(),// 已经含有Autoprefixer
+                                ]
+                            }
+                        },
+                        {
+                            loader: 'stylus-loader',
+                        }],
+                }),
             },
+            // ts
             {
-              loader: 'stylus-loader',
-            }],
+                test: /\.tsx?$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new CleanPlugin(['dist']),
+        // extractStylus,
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            allChunks: true,// 当使用提取公共模块插件时必须为true
         }),
-      },
-      // ts
-      {
-        test: /\.tsx?$/,
-        exclude: '/node_modules/',
-        use: {
-          loader: 'ts-loader',
-        },
-      },
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  plugins: [
-    new CleanPlugin(['dist']),
-    // extractStylus,
-    new ExtractTextPlugin({
-      filename: '[name].css',
-      allChunks: true,// 当使用提取公共模块插件时必须为true
-    }),
-  ],
 };
